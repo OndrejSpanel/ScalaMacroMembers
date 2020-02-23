51db74bdc55d60ac73043eeb1af8a870c499f161
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-02-23 13:47:39 +0100

    Member sum working.

--- members/src/main/scala/Macros.scala
+++ members/src/main/scala/Macros.scala
@@ -4,24 +4,34 @@ import scala.language.experimental.macros
 object Macros {
 
   def members[T]: Seq[String] = macro members_impl[T]
+  def sumMembers[T]: T => Int = macro sumMembers_impl[T]
 
-  def members_impl[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[Seq[String]] = {
+  private def members_list_impl[T: c.WeakTypeTag](c: blackbox.Context) = {
     import c.universe._
     val tType = weakTypeOf[T]
     val list = tType.decls.filter { f =>
       f.isMethod && f.asMethod.paramLists.isEmpty && f.asMethod.returnType == typeOf[Int]
-    }.map { m =>
-      m.name.decodedName.toString
     }
+    list
+  }
+  def members_impl[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[Seq[String]] = {
+    import c.universe._
+    val list = members_list_impl[T](c)
+    val memberNames = list.map(_.name.decodedName.toString)
     c.Expr[Seq[String]](
-      q"""Seq(..$list)"""
+      q"Seq(..$memberNames)"
     )
   }
 
-
-  def sumMembers[T]: T => Int = macro sumMembers_impl[T]
-
   def sumMembers_impl[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[T => Int] = {
-    ???
+    import c.universe._
+    val list = members_list_impl[T](c)
+    val tType = weakTypeOf[T]
+    val members = list.map { m =>
+      q"x.$m"
+    }
+    c.Expr[T => Int](
+      q"(x: $tType) => Seq(..$members).sum"
+    )
   }
 }
--- root/src/main/scala/Main.scala
+++ root/src/main/scala/Main.scala
@@ -1,11 +1,16 @@
 import Macros._
 
 object Main extends App {
-  class X {
-    val xa = 1
-    val xb = 2
-    val xc = 3
+  class X(a: Int = 1, b: Int = 2, c: Int = 3) {
+    val xa = a
+    val xb = b
+    val xc = c
   }
   val m = members[X]
+  val x = new X()
+  val y = new X(10, 100, 1000)
   println(s"X members: $m")
+  val sum = sumMembers[X]
+  println(s"x sum: ${sum(x)}")
+  println(s"y sum: ${sum(y)}")
 }
